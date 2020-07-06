05386a5c3d247272eb4b0565aca475377ea0d6cf
LiYu747 <1041924015@qq.com>
2020-07-06 15:48:52 +0800

    完成

--- server/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secret: 'lpwq1225',
-  whitePath: ['/', '/goods/home', '/goods/recommend', '/goods/allGoods', '/goods/detail', '/goods/search', '/users/login','/users/register']
-}
